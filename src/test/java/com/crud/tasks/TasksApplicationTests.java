36fec9b1174bdea4fb38229cb2472b56bd56669b
Joanna Kolodziejska <j.baranowska@gmail.com>
2018-02-15 11:52:38 +0100

    First attempt to Heroku

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +13,7 @@ public class TasksApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+
 
 }
