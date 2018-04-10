4657ad506f53d8a294a8de40445f94b7e9557bb0
Joanna Kolodziejska <j.baranowska@gmail.com>
2018-04-10 23:30:16 +0200

    Api_update

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -5,22 +5,28 @@ import com.crud.tasks.domain.TaskDto;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskMapperTest {
 
-    //@Autowired
+    @Autowired
     TaskMapper taskMapper;
 
-    @Before
+  /*  @Before
     public void setUp() throws Exception {
         taskMapper = new TaskMapper();
     }
-
+*/
     @Test
     public void mapToTask() throws Exception {
         //Given
